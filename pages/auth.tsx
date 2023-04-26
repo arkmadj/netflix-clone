1754b40631a0b00044cbd33b75ec6fb7aa330e30
arkmadj <ahmadjinadu@gmail.com>
2023-04-26 06:52:38 +0100

    added auth page

---
+++ pages/auth.tsx
@@ -0,0 +1,7 @@
+const Auth = () => {
+	return (
+		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed"></div>
+	);
+};
+
+export default Auth;
