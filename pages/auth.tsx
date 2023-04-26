ee1388b3cd25a336c84806bc36a840588e580b5b
arkmadj <ahmadjinadu@gmail.com>
2023-04-26 07:51:04 +0100

    added sigin in and login form

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,74 @@
+import Input from "@/components/Input";
+import { useCallback, useState } from "react";
+
 const Auth = () => {
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [password, setPassword] = useState("");
+
+	const [variant, setVariant] = useState("login");
+	const toggleVariant = useCallback(() => {
+		setVariant((currentVariant) =>
+			currentVariant === "login" ? "register" : "login"
+		);
+	}, []);
 	return (
-		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed"></div>
+		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
+			<div className="w-full h-full bg-black lg:bg-opacity-50">
+				<nav className="px-12 py-5">
+					<img src="/images/logo.png" alt="Logo" className="h-12" />
+				</nav>
+				<div className="flex justify-center">
+					<div className="self-center w-full px-16 py-16 mt-2 rounded-md bg-black/70 lg:w-2/5 lg:max-w-md">
+						<h2 className="mb-8 text-4xl font-semibold text-white">
+							{variant === "login" ? "Sign in" : "Register"}
+						</h2>
+						<div className="flex flex-col gap-4">
+							{variant === "register" && (
+								<Input
+									label="Username"
+									onChange={(e: any) => {
+										setName(e.target.value);
+									}}
+									id="name"
+									value={name}
+								/>
+							)}
+							<Input
+								label="Email"
+								onChange={(e: any) => {
+									setEmail(e.target.value);
+								}}
+								id="email"
+								type="email"
+								value={email}
+							/>
+							<Input
+								label="Password"
+								onChange={(e: any) => {
+									setPassword(e.target.value);
+								}}
+								id="password"
+								type="password"
+								value={password}
+							/>
+						</div>
+						<button className="w-full py-3 mt-10 text-white transition bg-red-600 rounded-md hover:bg-red-700">
+							{variant === 'login' ? 'Login' : 'Sign up'}
+						</button>
+						<p className="mt-12 text-neutral-500">
+							{variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+							<span
+								onClick={toggleVariant}
+								className="ml-1 text-white cursor-pointer hover:underline"
+							>
+								{variant === 'login' ? 'Create an account' : 'Login'}
+							</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
