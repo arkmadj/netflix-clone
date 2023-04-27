b5dfca8dffb04ec3e9b01974ae903d8a741650c4
arkmadj <ahmadjinadu@gmail.com>
2023-04-27 07:34:57 +0100

    added github signin

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,9 +2,20 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismadb from "@/lib/prismadb";
 import { compare } from "bcrypt";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export default NextAuth({
 	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID || "",
+			clientSecret: process.env.GITHUB_SECRET || "",
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID || "",
+			clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+		}),
 		Credentials({
 			id: "credentials",
 			name: "credentials",
@@ -46,12 +57,13 @@ export default NextAuth({
 			},
 		}),
 	],
-  pages: {
-    signIn: '/auth',
-  },
-  debug: process.env.NODE_ENV === 'development',
+	pages: {
+		signIn: "/auth",
+	},
+	debug: process.env.NODE_ENV === "development",
+	adapter: PrismaAdapter(prismadb),
 	session: {
-		strategy: 'jwt'
+		strategy: "jwt",
 	},
 	jwt: {
 		secret: process.env.NEXTAUTH_JWT_SECRET,
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,14 @@
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
+	const router = useRouter();
 	const [email, setEmail] = useState("");
 	const [name, setName] = useState("");
 	const [password, setPassword] = useState("");
@@ -15,6 +21,20 @@ const Auth = () => {
 		);
 	}, []);
 
+	const login = useCallback(async () => {
+		try {
+			await signIn("credentials", {
+				email,
+				password,
+				redirect: false,
+				callbackUrl: "/",
+			});
+			router.push("/");
+		} catch (error) {
+			console.log(error);
+		}
+	}, [email, password, router]);
+
 	const register = useCallback(async () => {
 		try {
 			await axios.post("/api/register", {
@@ -22,10 +42,12 @@ const Auth = () => {
 				name,
 				password,
 			});
+
+			login();
 		} catch (error) {
 			console.log(error);
 		}
-	}, [email, name, password]);
+	}, [email, name, password, login]);
 
 	return (
 		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
@@ -70,10 +92,21 @@ const Auth = () => {
 						</div>
 						<button
 							className="w-full py-3 mt-10 text-white transition bg-red-600 rounded-md hover:bg-red-700"
-							onClick={register}
+							onClick={variant === "login" ? login : register}
 						>
 							{variant === "login" ? "Login" : "Sign up"}
 						</button>
+						<div className="flex flex-row items-center justify-center gap-4 mt-8">
+							<div className="flex items-center justify-center w-10 h-10 transition bg-white rounded-full cursor-pointer hover:opacity-80">
+								<FcGoogle size={30} />
+							</div>
+							<div
+								onClick={() => signIn("github", { callbackUrl: "/" })}
+								className="flex items-center justify-center w-10 h-10 transition bg-white rounded-full cursor-pointer hover:opacity-80"
+							>
+								<FaGithub size={30} />
+							</div>
+						</div>
 						<p className="mt-12 text-neutral-500">
 							{variant === "login"
 								? "First time using Netflix?"
