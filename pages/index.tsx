62a4aa33da32b8d4a957e3e56c387b62cd6e43b3
arkmadj <ahmadjinadu@gmail.com>
2023-04-28 07:04:06 +0100

    added signout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
+import { signOut } from "next-auth/react";
+
 export default function Home() {
-  return (
-    <><h1>Netflix Clone</h1></>
-  )
+	return (
+		<>
+			<h1>Netflix Clone</h1>
+			<button onClick={() => signOut()} className="w-full h-10 bg-white">
+				Logout
+			</button>
+		</>
+	);
 }
