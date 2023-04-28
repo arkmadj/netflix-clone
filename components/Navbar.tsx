f92cd148e56cd0397cba6e51d6a6bc7f0f2fb1f3
arkmadj <ahmadjinadu@gmail.com>
2023-04-28 08:08:24 +0100

    added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import NavbarItem from "./NavbarItem";
+
+const Navbar = () => {
+	return (
+		<nav className="fixed z-40 w-full">
+			<div className="flex flex-row items-center px-4 py-6 transition duration-500 md:px-16 bg-zinc-900 bg-opacity-90">
+				<img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
+				<div className="flex-row hidden ml-8 gap-7 lg:flex">
+					<NavbarItem label="Home" />
+					<NavbarItem label="Series" />
+					<NavbarItem label="Films" />
+					<NavbarItem label="New & Popular" />
+					<NavbarItem label="Browse by languages" />
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
