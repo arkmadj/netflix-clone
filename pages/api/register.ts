c94170d47c511222c50ea9c48bc59cca868e57d8
arkmadj <ahmadjinadu@gmail.com>
2023-04-28 06:45:36 +0100

    added google auth

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -6,11 +6,10 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	if (req.method !== "POST") {
-		return res.status(405).end();
-	}
-
 	try {
+		if (req.method !== "POST") {
+			return res.status(405).end();
+		}
 		const { email, name, password } = req.body;
 
 		const existingUser = await prismadb.user.findUnique({
