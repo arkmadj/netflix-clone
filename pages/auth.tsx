8cbe61b8f60ce07f747bb18690ec07563befd154
arkmadj <ahmadjinadu@gmail.com>
2023-04-28 07:36:04 +0100

    redirect to profiles

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,13 +2,11 @@ import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 import axios from "axios";
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/router";
 
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
-	const router = useRouter();
 	const [email, setEmail] = useState("");
 	const [name, setName] = useState("");
 	const [password, setPassword] = useState("");
@@ -26,14 +24,12 @@ const Auth = () => {
 			await signIn("credentials", {
 				email,
 				password,
-				redirect: false,
-				callbackUrl: "/",
+				callbackUrl: "/profiles",
 			});
-			router.push("/");
 		} catch (error) {
 			console.log(error);
 		}
-	}, [email, password, router]);
+	}, [email, password]);
 
 	const register = useCallback(async () => {
 		try {
