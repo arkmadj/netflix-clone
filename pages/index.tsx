b087533cc7f430f6112ed1c8f881368f5b19b515
arkmadj <ahmadjinadu@gmail.com>
2023-04-28 07:24:43 +0100

    added access checked to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
-import { signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+	const session = await getSession(context);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: "/auth",
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser()
+
 	return (
 		<>
-			<h1>Netflix Clone</h1>
+			<h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as : {user?.name}</p>
 			<button onClick={() => signOut()} className="w-full h-10 bg-white">
 				Logout
 			</button>
