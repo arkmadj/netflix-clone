fd5aa5b6c81f10d4db08fb559176903bc5dc4836
arkmadj <ahmadjinadu@gmail.com>
2023-04-30 23:54:38 +0100

    added movie page

---
+++ pages/watch/[movieId].tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+import useMovie from "@/hooks/useMovie";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
+const Watch = () => {
+	const router = useRouter();
+	const { movieId } = router.query;
+
+	const { data } = useMovie(movieId as string);
+
+	return (
+		<div className="w-screen h-screen bg-black">
+			<nav className="fixed z-10 flex flex-row items-center w-full gap-8 p-4 bg-black bg-opacity-70">
+				<AiOutlineArrowLeft onclick={() => router.push('/')} className="text-white cursor-pointer" size={40} />
+				<p className="font-bold text-white text-1xl md:text-3xl">
+					<span className="font-light">Watching:</span>
+					{data?.title}
+				</p>
+			</nav>
+      <video className="w-full h-full" autoPlay controls src={data?.videoUrl}></video>
+		</div>
+	);
+};
+
+export default Watch;
